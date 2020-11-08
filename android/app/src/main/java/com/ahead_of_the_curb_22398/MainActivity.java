cc638a6d31bf1c1eac81037e784d060917aa863c
Crowdbotics <team@crowdbotics.com>
2020-11-08 09:14:03 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/ahead_of_the_curb_22398/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ahead_of_the_curb_22398;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+    * Returns the name of the main component registered from JavaScript. This is used to schedule
+    * rendering of the component.
+    */
+    @Override
+    protected String getMainComponentName() {
+        return "ahead_of_the_curb_22398";
+    }
+}
---
+++ ios/ahead_of_the_curb_22398Tests/ahead_of_the_curb_22398Tests.m
@@ -0,0 +1,30 @@
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+#import <React/RCTLog.h>
+#import <React/RCTRootView.h>
+
+#define TIMEOUT_SECONDS 600
+#define TEXT_TO_LOOK_FOR @"Welcome to React"
+
+@interface ahead_of_the_curb_22398 : XCTestCase
+
+@end
+
+@implementation ahead_of_the_curb_22398
+
+- (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
+{
+  if (test(view)) {
+    return YES;
+  }
+  for (UIView *subview in [view subviews]) {
+    if ([self findSubviewInView:subview matching:test]) {
+      return YES;
+    }
+  }
+  return NO;
+}
+
+
+@end
